21ed776409e000f85a723139a50a4c122083f83b
mohsen-zahed <mohsenzahed0077@gmail.com>
2023-02-28 16:00:27 +0430

    updated

---
+++ lib/constants/list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+List<Map<String, dynamic>> skillsList = [
+  {
+    'title': 'Photoshop',
+    'image': 'assets/images/app_icon_01.png',
+    'color': Colors.blue,
+    'isSelected': false,
+  },
+  {
+    'title': 'Adobe XD',
+    'image': 'assets/images/app_icon_05.png',
+    'color': Colors.pink,
+    'isSelected': false,
+  },
+  {
+    'title': 'Lightroom',
+    'image': 'assets/images/app_icon_02.png',
+    'color': Colors.lightBlue,
+    'isSelected': false,
+  },
+  {
+    'title': 'After Effect',
+    'image': 'assets/images/app_icon_03.png',
+    'color': Colors.deepPurple,
+    'isSelected': false,
+  },
+  {
+    'title': 'Illustrator',
+    'image': 'assets/images/app_icon_04.png',
+    'color': Colors.orange,
+    'isSelected': false,
+  },
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:professional_id_card/constants/list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         brightness: Brightness.dark,
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          border: InputBorder.none,
+        ),
       ),
       home: const MyHomePage(),
     );
@@ -31,17 +37,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isCollapsed = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Professional ID Card'),
-        actions: [
+        title: const Text('Professional ID Card'),
+        actions: const [
           Icon(Icons.bubble_chart_outlined),
           SizedBox(width: 10),
           Icon(Icons.more_vert_rounded),
           SizedBox(width: 15),
         ],
+        elevation: 0,
       ),
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
@@ -114,40 +122,187 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Divider(),
             ),
             const SizedBox(height: 15),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  isCollapsed = !isCollapsed;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30, 10, 30, 20),
+                child: Row(
+                  children: [
+                    const Text('Skills'),
+                    !isCollapsed
+                        ? const Icon(
+                            Icons.keyboard_arrow_down_rounded,
+                            size: 18,
+                          )
+                        : const Icon(
+                            Icons.keyboard_arrow_right_rounded,
+                            size: 18,
+                          ),
+                  ],
+                ),
+              ),
+            ),
+            !isCollapsed
+                ? Wrap(
+                    spacing: 7,
+                    runSpacing: 10,
+                    direction: Axis.horizontal,
+                    children: [
+                      ...List.generate(
+                        skillsList.length,
+                        (index) => SkillWidget(
+                          color: skillsList[index]['color'],
+                          image: skillsList[index]['image'],
+                          isSelected: skillsList[index]['isSelected'],
+                          title: skillsList[index]['title'],
+                          onTap: () {
+                            //...
+                            setState(() {
+                              skillsList[index]['isSelected'] =
+                                  !skillsList[index]['isSelected'];
+                            });
+                          },
+                        ),
+                      )
+                    ],
+                  )
+                : const SizedBox(),
+            const Divider(),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Row(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Skills'),
-                  Icon(
-                    Icons.keyboard_arrow_down_rounded,
-                    size: 18,
+                  const Text(
+                    'Personal Information',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
+                  const SizedBox(height: 20),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.alternate_email_rounded,
+                          color: Colors.grey,
+                          size: 20,
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                        hintText: 'Email',
+                        hintStyle: TextStyle(fontSize: 14),
+                      ),
+                      cursorColor: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.grey,
+                          size: 20,
+                        ),
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                        hintText: 'Password',
+                        hintStyle: TextStyle(fontSize: 14),
+                      ),
+                      cursorColor: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        //...
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          Colors.pink[400],
+                        ),
+                      ),
+                      child: Text('Save'),
+                    ),
+                  )
                 ],
               ),
             ),
-            Wrap(
-              direction: Axis.horizontal,
-              children: [
-                Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    color: Color(0x0dffffff),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SkillWidget extends StatelessWidget {
+  const SkillWidget({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.color,
+    required this.onTap,
+    required this.isSelected,
+  }) : super(key: key);
+  final String image;
+  final String title;
+  final Color color;
+  final Function() onTap;
+  final bool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(15),
+      onTap: onTap,
+      child: Container(
+        width: 120,
+        height: 120,
+        decoration: isSelected
+            ? BoxDecoration(
+                color: const Color(0x0dffffff),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: color.withOpacity(0.4),
+                    offset: const Offset(0, 1),
+                    blurRadius: 70,
                   ),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/images/app_icon_01.png',
-                        width: 40,
-                      ),
-                      const SizedBox(height: 8),
-                      const Text('Photoshop'),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              )
+            : const BoxDecoration(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: isSelected
+                  ? BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: color,
+                          offset: const Offset(0, 1),
+                          blurRadius: 20,
+                        ),
+                      ],
+                    )
+                  : const BoxDecoration(),
+              child: Image.asset(
+                image,
+                width: 40,
+              ),
             ),
+            const SizedBox(height: 8),
+            Text(title),
           ],
         ),
       ),
